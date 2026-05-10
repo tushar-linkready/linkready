344283956b5b8e9211a602387aecc3f11fe479c1
tushar-linkready <caaroratushar@gmail.com>
2026-05-10 07:15:51 +0000

    Fix truncated next.config.js

--- next.config.js
+++ next.config.js
@@ -9,8 +9,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*.cdninstagram.com',
+        protocol: "https",
+        hostname: "*.cdninstagram.com",
       },
       {
-        protocol:
+        protocol: "https",
+        hostname: "*.fbcdn.net",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
